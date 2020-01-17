27f735f33818742eb92c3f4f3240d608f53d0e34
Lingxi Li <lilingxi01@gmail.com>
2020-01-17 18:48:01 -0500

    Optimize the drawing method

--- roundedProgressBarModule/src/main/java/cn/tacitech/roundedprogressbarmodule/RoundedProgressBar.java
+++ roundedProgressBarModule/src/main/java/cn/tacitech/roundedprogressbarmodule/RoundedProgressBar.java
@@ -5,26 +5,38 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.Region;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 
 public class RoundedProgressBar extends View {
 
-    private int roundedRadius = -1;
+    private float roundedRadius = -1;
     private int progress = 0;
     private int barBackgroundColor = 0;
     private int barProgressColor = 0;
     private int drawingDirection = 0;
 
     private Paint mPaint;
+    private Path mPath;
 
     public RoundedProgressBar(Context context) {
         this(context, null);
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPath = new Path();
     }
 
     public RoundedProgressBar(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPath = new Path();
     }
 
     public RoundedProgressBar(Context context, AttributeSet attrs, int defStyleAttr){
@@ -33,6 +45,7 @@ public class RoundedProgressBar extends View {
 
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
+        mPath = new Path();
     }
 
     @Override
@@ -44,8 +57,8 @@ public class RoundedProgressBar extends View {
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if(roundedRadius < 0 || roundedRadius > canvas.getWidth() / 2 || roundedRadius > canvas.getHeight() / 2){
-            roundedRadius = (canvas.getWidth() > canvas.getHeight()) ? canvas.getHeight() / 2 : canvas.getWidth() / 2;
+        if(roundedRadius < 0f || roundedRadius > canvas.getWidth() / 2f || roundedRadius > canvas.getHeight() / 2f){
+            roundedRadius = (canvas.getWidth() > canvas.getHeight()) ? canvas.getHeight() / 2f : canvas.getWidth() / 2f;
         }
 
         if(barBackgroundColor == 0){
@@ -63,53 +76,41 @@ public class RoundedProgressBar extends View {
 
         // 横向绘制
         if(drawingDirection == DrawingDirection.HORIZONTAL) {
-            int startPointX = roundedRadius;
-            int startPointY = roundedRadius;
-            int endPointBackgroundX = canvas.getWidth() - roundedRadius;
-            int endPointProgressX = (canvas.getWidth() - roundedRadius * 2) * progress / 100 + roundedRadius;
-            int endPointY = roundedRadius;
-
-            mPaint.setStyle(Paint.Style.STROKE);
-            mPaint.setStrokeCap(Paint.Cap.ROUND);
-            mPaint.setColor(barBackgroundColor);
-            mPaint.setStrokeWidth(roundedRadius * 2);
-
             // 绘制背景
-            canvas.drawLine(startPointX, startPointY, endPointBackgroundX, endPointY, mPaint);
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(barBackgroundColor);
+            RectF rectF_background = new RectF(0, 0, canvas.getWidth(), canvas.getHeight());
+            canvas.drawRoundRect(rectF_background, roundedRadius, roundedRadius, mPaint);
+
+            mPath.addRoundRect(rectF_background, roundedRadius, roundedRadius,
+                    Path.Direction.CW);
+            canvas.clipPath(mPath, Region.Op.INTERSECT);
 
             // 绘制进度
-            if(progress != 0) {
-                mPaint.setStyle(Paint.Style.STROKE);
-                mPaint.setStrokeCap(Paint.Cap.ROUND);
-                mPaint.setColor(barProgressColor);
-                mPaint.setStrokeWidth(roundedRadius * 2);
-                canvas.drawLine(startPointX, startPointY, endPointProgressX, endPointY, mPaint);
-            }
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(barProgressColor);
+            RectF rectF_progress = new RectF(roundedRadius * -1f, 0,
+                    canvas.getWidth() * progress / 100, canvas.getHeight());
+            canvas.drawRoundRect(rectF_progress, roundedRadius, roundedRadius, mPaint);
         }
 
         if(drawingDirection == DrawingDirection.VERTICAL){
-            int startPointX = roundedRadius;
-            int startPointY = canvas.getHeight() - roundedRadius;
-            int endPointX = roundedRadius;
-            int endPointBackgroundY = roundedRadius;
-            int endPointProgressY = (canvas.getHeight() - roundedRadius * 2) * (100 - progress) / 100 + roundedRadius;
-
-            mPaint.setStyle(Paint.Style.STROKE);
-            mPaint.setStrokeCap(Paint.Cap.ROUND);
-            mPaint.setColor(barBackgroundColor);
-            mPaint.setStrokeWidth(roundedRadius * 2);
-
             // 绘制背景
-            canvas.drawLine(startPointX, startPointY, endPointX, endPointBackgroundY, mPaint);
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(barBackgroundColor);
+            RectF rectF_background = new RectF(0, 0, canvas.getWidth(), canvas.getHeight());
+            canvas.drawRoundRect(rectF_background, roundedRadius, roundedRadius, mPaint);
+
+            mPath.addRoundRect(rectF_background, roundedRadius, roundedRadius,
+                    Path.Direction.CW);
+            canvas.clipPath(mPath, Region.Op.INTERSECT);
 
             // 绘制进度
-            if(progress != 0) {
-                mPaint.setStyle(Paint.Style.STROKE);
-                mPaint.setStrokeCap(Paint.Cap.ROUND);
-                mPaint.setColor(barProgressColor);
-                mPaint.setStrokeWidth(roundedRadius * 2);
-                canvas.drawLine(startPointX, startPointY, endPointX, endPointProgressY, mPaint);
-            }
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(barProgressColor);
+            RectF rectF_progress = new RectF(roundedRadius * -1f, 0,
+                    canvas.getWidth() * progress / 100, canvas.getHeight());
+            canvas.drawRoundRect(rectF_progress, roundedRadius, roundedRadius, mPaint);
         }
     }
 
@@ -118,13 +119,15 @@ public class RoundedProgressBar extends View {
 
         try {
             roundedRadius =
-                    attrs.getInt(R.styleable.RoundedProgressBar_roundedRadius, -1);
+                    attrs.getDimension(R.styleable.RoundedProgressBar_roundedRadius, -1f);
             progress =
                     attrs.getInt(R.styleable.RoundedProgressBar_progress, -1);
             barBackgroundColor =
                     attrs.getColor(R.styleable.RoundedProgressBar_barBackgroundColor, 0);
             barProgressColor =
                     attrs.getColor(R.styleable.RoundedProgressBar_barProgressColor, 0);
+            drawingDirection =
+                    attrs.getInt(R.styleable.RoundedProgressBar_drawingDirection, 0);
         } finally {
             attrs.recycle();
         }
@@ -140,7 +143,7 @@ public class RoundedProgressBar extends View {
      * @param radius
      */
     public void setRoundedRadius(int radius){
-        this.roundedRadius = (int) dpToPixel(radius);
+        this.roundedRadius = dpToPixel(radius);
         invalidate();
     }
 
